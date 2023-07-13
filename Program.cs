e564c1f90d06f4ccd971e81bfb3c93ed7a62e1b3
ashishrathore98 <ashrathore98@gmail.com>
2023-07-13 14:55:01 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAssignment04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Check if the number is positive, negative, or zero
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+
+            // Check if the number is even or odd using the conditional operator
+            string parity = (number % 2 == 0) ? "even" : "odd";
+            Console.WriteLine("The number is " + parity + ".");
+
+        }
+    }
+}
